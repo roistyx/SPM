f31972881bf1a4fbc8c0e82985dc8b37813665cc
Roie A <roee.abram@gmail.com>
2024-03-13 11:14:31 -0400

    # Changes to be committed: #	modified:   package.json #	deleted:    src/components/ArticleEditor.css #	deleted:    src/components/ArticleEditor.js #	deleted:    src/components/BarChartComponent.js #	deleted:    src/components/ChatGBT.js #	deleted:    src/components/ChatGPT.css #	modified:   src/components/CustomCalendar/CustomCalendar.js #	deleted:    src/components/DatesPicker.css #	deleted:    src/components/DatesPicker.js #	deleted:    src/components/LineChartComponent.js #	deleted:    src/views/StockFinancials.js

--- src/components/CustomCalendar/CustomCalendar.js
+++ src/components/CustomCalendar/CustomCalendar.js
@@ -1,24 +1,20 @@
-import React, { useState } from 'react';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
-import format from 'date-fns/format';
-import dayjs from 'dayjs';
+import React, { useState } from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import format from "date-fns/format";
+import dayjs from "dayjs";
 
 export default function BasicDateCalendar() {
   const [dateValue, setDateValue] = useState(new Date());
 
   const handleDateChange = (newDate) => {
     setDateValue(newDate);
-    console.log(format(newDate, 'PPP')); // Example of logging the date in a readable format
+    console.log(format(newDate, "PPP")); // Example of logging the date in a readable format
   };
 
   // Set the minimum date to the start of the current month using date-fns
-  const minDate = new Date(
-    new Date().getFullYear(),
-    new Date().getMonth(),
-    1
-  );
+  const minDate = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
 
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
@@ -26,7 +22,7 @@ export default function BasicDateCalendar() {
         date={dateValue}
         onChange={handleDateChange}
         minDate={minDate}
-        views={['day']}
+        views={["day"]}
         openTo="day"
       />
     </LocalizationProvider>
