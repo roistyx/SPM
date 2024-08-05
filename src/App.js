937065f92c4e72db6b100e3b21f2b1dc51238a2a
Roie <roee.abram@gmail.com>
2024-08-05 12:16:37 -0400

    UPDATED: Removed ColorTheme component and its related CSS luxon.
    UPDATED: Removed <ColorTheme /> route from Routes in App.js.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { useEffect, useRef, useState } from "react";
-import { BrowserRouter } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
-import Stepper from "./features/Stepper/Stepper.js";
-import HeroSection from "./components/HeroSection/HeroSection";
-import ColorTheme from "./components/ColorTheme/ColorTheme";
-import Header from "./components/Header/Header.js";
-import Footer from "./components/Footer/Footer.js";
-import Calendar from "./components/NewCalendar/NewCalendar";
-import PersonalPathologyForm from "./components/Appointment/PersonalPathologyForm";
+import React, { useEffect, useRef, useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Stepper from './features/Stepper/Stepper.js';
+import HeroSection from './components/HeroSection/HeroSection';
+import Header from './components/Header/Header.js';
+import Footer from './components/Footer/Footer.js';
+import Calendar from './components/NewCalendar/NewCalendar';
+import PersonalPathologyForm from './components/Appointment/PersonalPathologyForm';
 
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
@@ -20,7 +19,6 @@ function App() {
         <div className="background-image">
           <Routes>
             <Route path="/" element={<HeroSection />} />
-            <Route path="/colors" element={<ColorTheme />} />
             <Route path="/appointments" element={<Stepper />} />
             <Route path="/new-calendar" element={<Calendar />} />
             <Route path="/form" element={<PersonalPathologyForm />} />
